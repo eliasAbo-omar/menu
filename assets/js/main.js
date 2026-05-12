6ebcca42a0e60a8d55374d49fa99ea0ea276fcad
Elias <ekasa123@gmail.com>
2026-05-12 15:32:46 +0300

    finish menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
 const navBar = document.querySelector(".nav-bar");
 const menuButton = document.querySelector(".menu-button");
+const navLinks = document.querySelectorAll(".nav-bar li a");
 
 menuButton.addEventListener("click", () => {
   navBar.classList.toggle("open");
   menuButton.classList.toggle("open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navBar.classList.remove("open");
+    menuButton.classList.remove("open");
+  });
+});
+
+document.addEventListener("click", (e) => {
+  if (!navBar.contains(e.target) && !menuButton.contains(e.target)) {
+    navBar.classList.remove("open");
+    menuButton.classList.remove("open");
+  }
 });
